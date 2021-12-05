7606ec85e956f5c2e5f73c8238f8c958afe13e19
지서향 <@DESKTOP-T6I33JE>
2021-12-05 20:54:01 +0900

    2021-12-05 commit-test shji

--- BasicGrammar/src/chapter0/test/AllTests.java
+++ BasicGrammar/src/chapter0/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ Tests.class, Tests2.class, Tests3.class })
+@SuiteClasses({ Tests.class, Tests2.class, Tests3.class, Tests4.class})
 public class AllTests {
 
 }
---
+++ BasicGrammar/src/chapter0/test/Tests4.java
@@ -0,0 +1,14 @@
+package chapter0.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Tests4 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
