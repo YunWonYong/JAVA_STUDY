4e26104279f5b9c232993030a352acc77603db3e
ywyi1992 <ywyi1992@naver.com>
2021-12-19 14:07:14 +0900

    2021-12-19 chapter1 init

---
+++ BasicGrammar/src/chapter1/test/VariablesTest.java
@@ -0,0 +1,14 @@
+package chapter1.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VariablesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
